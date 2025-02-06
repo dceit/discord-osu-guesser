042ee03480919ee82670a97d300a183f3fed1314
dceit <72780993+dceit@users.noreply.github.com>
2025-02-06 15:56:12 -0500

    Implement api.getUserByQuery()

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -1,6 +1,41 @@
 import axios from 'axios';
 const { API_BASE_DOMAIN, API_TOKEN } = process.env
 
+export function getUserByQuery(
+	query,
+	limit=1,
+	endpoint='/api/users/search'
+	) {
+
+	if(query == null) {
+		return null;
+	}
+
+	return new Promise(async (resolve, reject) => {
+		await axios.get(API_BASE_DOMAIN + endpoint,
+	    	{
+	    		headers: { 'X-API-Key': API_TOKEN, },
+	    		params: {
+			        query: query,
+			        limit: limit
+			    }
+	    	})
+	        .then(response => {
+	        	if(!response.data.success)
+	        		return reject('ERROR: API returned failed success. Aborting.')
+
+	        	if(!response.data.data)
+	        		return reject('ERROR: Data not returning from endpoint. Aborting.')
+
+	        	if(response.data.data.length == 0)
+	        		return reject('ERROR: No users found with this query. Aborting.')
+
+	        	return resolve(response.data.data[0]);
+	        })
+	        .catch(error => reject(`ERROR: Something went wrong. Is the website online?`));
+	});
+}
+
 export function getLeaderBoardData(
 		mode, 
 		variant,

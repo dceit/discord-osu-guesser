21963c2254fb578e5e5bdb6979a581e1f0443d2f
dceit <72780993+dceit@users.noreply.github.com>
2025-02-07 19:56:33 -0500

    Implement role creation and assignment

--- src/Commands/osuguesser/connect.js
+++ src/Commands/osuguesser/connect.js
@@ -6,6 +6,15 @@ import { getUserByQuery, getUserByBanchoID } from '../../Helpers/api.js';
   TODO:
     Implement custom oAuth authentication rather than
     trusting who the user is.
+
+  TODO:
+    Removal of roles when new connection is made?
+
+  TODO:
+    Move away from traditional try {} catch{}, this
+    always comes off as really hacky for me...
+    Why not handle errors myself instead of letting them
+    happen in the first place...
 */
 
 export const commandBase = {
@@ -37,7 +46,35 @@ export const commandBase = {
       .then(async response => {
         await getUserByBanchoID(response.bancho_id)
           .then(async user => {
-            console.log(user)
+            const { guild } = interaction
+            const { badges } = user
+
+            for(let badge of badges) {
+              let role = guild.roles.cache.find(r => r.name === badge.name)
+
+              if (!role) {
+                // If the role doesn't exist, create it
+                try {
+                  role = await guild.roles.create({
+                    name: badge.name,
+                    color: badge.color,
+                    reason: `Created role for ${badge.name} badge.`,
+                  })
+                } catch (error) {
+                  return interaction.reply(`An error occurred while creating the ${badge.name} role.`)
+                }
+              }
+
+              const member = await guild.members.fetch(interaction.user.id);
+
+              // Hacky logic for assigning roles. This should be better...
+              try {
+                await member.roles.add(role);
+              } catch (error) {
+                return interaction.reply(`An error occurred while assigning the ${badge.name} role.`);
+              }
+            }
+
             return interaction.reply(`Connected to account https://osu.ppy.sh/u/${user.bancho_id}`);
           })
           .catch(() => {
